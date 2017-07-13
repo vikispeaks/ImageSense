0a179a04ab984f686dda3ac88d39570cb74ac203
Vignesh Rajendran <vign.ran@gmail.com>
2017-07-13 15:08:34 +0530

    Base Changes with Full UI

---
+++ www/js/controllers.js
@@ -0,0 +1,98 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reportsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('consultationsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('philipsImageSenseCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('healthWellbeingCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('additionalDetailsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cameraGalleryLandingPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('reportDetailCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('consultantListCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('bookAppointmentCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,131 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('philipsImageSense.home', {
+    url: '/page1',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.reports', {
+    url: '/page2',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/reports.html',
+        controller: 'reportsCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.consultations', {
+    url: '/page3',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/consultations.html',
+        controller: 'consultationsCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense', {
+    url: '/side-menu21',
+    templateUrl: 'templates/philipsImageSense.html',
+    controller: 'philipsImageSenseCtrl'
+  })
+
+  .state('philipsImageSense.healthWellbeing', {
+    url: '/page4',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/healthWellbeing.html',
+        controller: 'healthWellbeingCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.login', {
+    url: '/page5',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/login.html',
+        controller: 'loginCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.signup', {
+    url: '/page6',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/signup.html',
+        controller: 'signupCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.additionalDetails', {
+    url: '/page7',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/additionalDetails.html',
+        controller: 'additionalDetailsCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.cameraGalleryLandingPage', {
+    url: '/page8',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/cameraGalleryLandingPage.html',
+        controller: 'cameraGalleryLandingPageCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.reportDetail', {
+    url: '/page10',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/reportDetail.html',
+        controller: 'reportDetailCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.consultantList', {
+    url: '/page11',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/consultantList.html',
+        controller: 'consultantListCtrl'
+      }
+    }
+  })
+
+  .state('philipsImageSense.bookAppointment', {
+    url: '/page12',
+    views: {
+      'side-menu21': {
+        templateUrl: 'templates/bookAppointment.html',
+        controller: 'bookAppointmentCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/side-menu21/page1')
+
+
+});
